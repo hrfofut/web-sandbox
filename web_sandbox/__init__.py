2a1056c9f64d136278cb58527870875f6cdb947e
Harrison Foreman <harrisonrforeman@gmail.com>
2019-02-13 00:52:32 -0600

    Added old wordCheat tool and updated it slightly.

--- web_sandbox/__init__.py
+++ web_sandbox/__init__.py
@@ -7,7 +7,7 @@ from .views import misc
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_url_path='/static')
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
--- web_sandbox/views.py
+++ web_sandbox/views.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
 
+
 misc = Blueprint('misc', __name__)
 
 @misc.route("/")
 def splash(name=None):
 	return render_template('home.html')
+
+@misc.route("/wordcheat")
+def cheat(name=None):
+	f = open("web_sandbox/static/ospd.txt")
+	return render_template('wordCheat.html', wordlist = f.read().splitlines())
